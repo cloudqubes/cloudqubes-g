6e1a13ccf79a6cc67876922e89ab0310b8ca4a36
cloudqubes <cloud.qubes@gmail.com>
2023-12-20 04:01:58 +0530

    new analytics stream

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     options: {
       // You can add multiple tracking ids and a pageview event will be fired for all of them.
       trackingIds: [
-        "G-NSEVGKVG1W", // Google Analytics / GA
+        "G-YKJN6RN811", // Google Analytics / GA
         // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
         // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
       ],
